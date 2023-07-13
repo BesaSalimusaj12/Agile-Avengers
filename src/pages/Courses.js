f8bda085f32c28626d3c97704b3d5ac7d8cc8cec
rrexhmati1999 <ronaldorexhmati@gmail.com>
2023-07-13 16:22:17 +0200

    Changes to commit.

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,26 +1,57 @@
 import React from 'react'
 import "../styles/Courses.css";
 import CoursesBackground from "../assets/courses.jpg"
+import { Link } from 'react-router-dom';
+
 
 export default function Courses() {
-    const courses = [
-        'Introduction to Computer Science',
-        'Data Structures and Algorithms',
-        'Web Development Fundamentals',
-        'Database Design and Management',
-        'Software Engineering Principles',
-      ];
   return (
     <div className="courses">
       <div style={{ backgroundImage: `url(${CoursesBackground})` }}
         className="coursesTop"
-      ></div>
+      >
+        <div className="text-overlay">
+          <h1 className="text">KURSET</h1>
+        </div>
+      </div>
+
       <div className="coursesBottom">
-        <h1>KURSET</h1>
-        <ul>
-          {courses.map((course, index) => (
-            <li key={index}>{course}</li>
-          ))}
+      <ul className="category-list">
+        <li>
+          <Link to="/category1" className="category-link">
+            Category 1
+          </Link>
+        </li>
+        <li>
+          <Link to="/category2" className="category-link">
+            Category 2
+          </Link>
+        </li>
+        <li>
+          <Link to="/category3" className="category-link">
+            Category 3
+          </Link>
+        </li>
+        <li>
+          <Link to="/category4" className="category-link">
+            Category 4
+          </Link>
+        </li>
+        <li>
+          <Link to="/category5" className="category-link">
+            Category 5
+          </Link>
+        </li>
+        <li>
+          <Link to="/category6" className="category-link">
+            Category 6
+          </Link>
+        </li>
+        <li>
+          <Link to="/category7" className="category-link">
+            Category 7
+          </Link>
+        </li>
         </ul>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
